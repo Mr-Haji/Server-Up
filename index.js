85cc2db00fdc762516756ab90fb7f6eb15d65cb3
Mr-Haji <abraheemofficial@gmail.com>
2024-03-04 23:45:39 +0500

    New Work

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 7860;
 
 
 app.listen(PORT, () => {
-    console.log(`Server Is Upp On http://localhost:${PORT} Hello Backend `)
+  console.log(`Server Is Upp On http://localhost:${PORT} Hello Backend `)
 })
 
 // app.get("", (request, response) => {
@@ -15,22 +15,49 @@ app.listen(PORT, () => {
 //     })
 // })
 // --------------------------------------
-// app.get("/home", (request, response) => {
+// app.get("/", (request, response) => {
 
 //     response.send(Data)
 // })
 // ---------------------------------
-app.get("/home/:id", (request, response) => {
-    console.log("request", request.query)
-    if (request.query) {
-        const FilerData = Data.filter((product) => { product.id == request.query }
-        )
-        response.send(FilerData)
-    }
-    else (response.send(Data))
+// app.get("", (request, response) => {
+//     console.log("request", request.query)
+//     if (request.query) {
+//         const FilerData = Data.filter((product) => { product.id == request.query.id }
+//         )
+//         response.send(FilerData)
+//         return
+//     }
+//     else (response.send(Data))
+// })
 
+// by using normal patram get all products
+// app.get("/:id", (request, response) => {
+//   const a = request.params
+//   console.log("a", a);
+//   if (a) {
+//     const filterData = Data.filter(
+//       (product) => product.id == a.id
+//     );
+//     response.send(filterData);
+//     return;
+//   }
 
-})
+//   response.send(Data);
+// });
+app.get("/", (request, response) => {
+  const Q = request.query
+  console.log("Q", Q);
+  if (Q.id) {
+    const filterData = Data.filter(
+      (product) => product.id == Q.id
+    );
+    response.send(filterData);
+    return;
+  }
+
+  response.send(Data);
+});
 
 // console.log('Check The App')
 
